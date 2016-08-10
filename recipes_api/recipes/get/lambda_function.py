66147ec8108a598396108b7417dac0ca28818c23
Peter Mitrano <mitranopeter@gmail.com>
2016-08-10 00:35:06 -0400

    added basic recipe get and requirements.txt
    next I will write tests to for id input validation

--- recipes_api/recipes/get/lambda_function.py
+++ recipes_api/recipes/get/lambda_function.py
@@ -1,7 +1,25 @@
 import json
+import boto3
 
 def handle_event(event, context):
-    print context
-    print event
-    return "get recipe"
+    # parse the recipe id from the parameters
+    try:
+        recipe_id =  event['params']['querystring']['id']
 
+        # lookup that id in our database
+        resource = boto3.resource("dynamodb")
+
+        try:
+            id_num = int(recipe_id)
+
+            table = resource.Table('my_cookbook_recipes')
+            response = table.get_item(Key={'id': id_num})
+
+            if 'Item' in response:
+                return response['Item']
+            else:
+                return "No recipe ith id %d" % id_num
+        except ValueError:
+            return "recipe_id must be an integer"
+    except KeyError:
+        return "Failed to parse query prameter 'id'"
