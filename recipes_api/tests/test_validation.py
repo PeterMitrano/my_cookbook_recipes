9bf41e916c2eb470a1fa83e05ef396fca1e61f7a
Peter Mitrano <mitranopeter@gmail.com>
2016-08-12 16:12:24 -0400

    changed out CONTEXT dict for None
    using environment variable to define debug.

--- recipes_api/tests/test_validation.py
+++ recipes_api/tests/test_validation.py
@@ -1,12 +1,11 @@
 import logging
+import os
 import random
 import unittest
 
 from recipes_api.recipes.get import lambda_function
 
 logging.getLogger('nose').setLevel(logging.WARNING)
-
-CONTEXT = {"debug": True}
 
 
 class ValidationTest(unittest.TestCase):
@@ -17,14 +16,14 @@ class ValidationTest(unittest.TestCase):
         for i in range(0, 100, 5):
             logging.getLogger("TEST").info(i)
             event = self.make_event(i)
-            response = lambda_function.handle_event(event, CONTEXT)
+            response = lambda_function.handle_event(event, None)
             self.assertGreaterEqual(response['code'], 0)
 
     def test_invalid_int(self):
         for i in range(-100, -1, 5):
             logging.getLogger("TEST").info(i)
             event = self.make_event(i)
-            response = lambda_function.handle_event(event, CONTEXT)
+            response = lambda_function.handle_event(event, None)
             self.assertLess(response['code'], 0)
             self.assertEqual(type(response['data']), str)
 
@@ -32,6 +31,18 @@ class ValidationTest(unittest.TestCase):
         for i in range(10):
             recipe_id = random.random() * 100 - 50
             event = self.make_event(recipe_id)
-            response = lambda_function.handle_event(event, CONTEXT)
+            response = lambda_function.handle_event(event, None)
             self.assertLess(response['code'], 0)
             self.assertEqual(type(response['data']), str)
+
+    def test_str(self):
+        event = self.make_event('evil_input_string')
+        response = lambda_function.handle_event(event, None)
+        self.assertLess(response['code'], 0)
+        self.assertEqual(type(response['data']), str)
+
+    def test_dict(self):
+        event = self.make_event({'evil': 'input_dict'})
+        response = lambda_function.handle_event(event, None)
+        self.assertLess(response['code'], 0)
+        self.assertEqual(type(response['data']), str)
